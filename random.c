ec2486cf601e146e5a0a98cbb3442fbd23fd686b
SivhourLe-TU <sivhour.le@temple.edu>
2026-08-26 10:07:20 -0400

    Added random.c

---
+++ random.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* rand_string() is a character function that returns a string
+of random characters (a word). The length of the word is a
+parameter in your program */
+
+void rand_string(char *s, size_t size) {
+
+}
